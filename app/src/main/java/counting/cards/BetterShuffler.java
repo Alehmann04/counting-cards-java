61b565a081d5e3d5bbe830302e0ef11dd56308dc
Kamui_Kazi <xXKamuiKaziXx@gmail.com>
2025-09-10 10:08:10 -0500

    better suffle complete

---
+++ app/src/main/java/counting/cards/BetterShuffler.java
@@ -0,0 +1,38 @@
+package counting.cards;
+
+import java.util.Random;
+
+public class BetterShuffler {
+
+  Random rand = new Random();
+  int pointer = 0;
+
+  int[] push(int[] deck, int value) {
+    deck[pointer] = value;
+    this.pointer++;
+    return deck;
+  }
+
+  int[] splice(int[] deck, int index) {
+    int[] output = new int[deck.length - 1];
+    for (int i = 0; i < index; i++) {
+      output[i] = deck[i];
+    }
+    for (int i = index + 1; i < deck.length; i++) {
+      output[i - 1] = deck[i];
+    }
+    return output;
+  }
+
+  public int[] BetterShuffle(int[] deck) {
+    int[] shuffle = new int[deck.length];
+    int n = deck.length;
+    while (n > 0) {
+      int i = rand.nextInt(n);
+      shuffle = push(shuffle, deck[i]);
+      deck = splice(deck, i);
+      n--;
+    }
+    return shuffle;
+  }
+}
